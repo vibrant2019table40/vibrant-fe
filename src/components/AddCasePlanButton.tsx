7cf2784ac887f0ad6f8e19d5df8ae6719df9b33e
Steve Ortiz <steve@ortizaggies.com>
2019-12-02 17:32:40 -0800

    Change id generation

--- src/components/AddCasePlanButton.tsx
+++ src/components/AddCasePlanButton.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import {Button} from 'antd'
-import {useHistory} from 'react-router'
-import shortid from 'shortid'
+import { Button } from 'antd'
+import { useHistory } from 'react-router'
+
+function makeid() {
+    var result = '';
+    const idLength = 5;
+    const characters = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
+    const charactersLength = characters.length;
+    for (var i = 0; i < idLength; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 const AddCasePlanButton = () => {
     const history = useHistory()
 
     return (
-        <Button onClick={() => history.push(`/${shortid.generate()}`)}>Start a new case plan</Button>
+        <Button onClick={() => history.push(`/${makeid()}`)}>Start a new case plan</Button>
     )
 }
 
