832de44ac1c912c03273c0e70a4ac709dd9d72e8
Brian Murphey <bmurphey@bradsdeals.com>
2019-12-02 17:29:44 -0800

    Update setup, gitignore, and add S3 policy and CloudFront config

--- setup.sh
+++ setup.sh
@@ -5,6 +5,12 @@ bucket=$1
 aws --profile vibrant s3 ls "s3://${bucket}"
 
 if [[ $? -ne 0 ]]; then
-    aws --profile s3 mb "s3://${bucket}" --region "us-east-2"
-    aws --profile vibrant s3 website "s3://${bucket}/" --index-document index.html --error-document error.html
+    aws --profile vibrant s3 mb "s3://${bucket}" --region "us-east-1"
+    aws --profile vibrant s3 website "s3://${bucket}/" --index-document index.html --error-document index.html
+fi
+
+aws --profile vibrant s3 ls "s3://${bucket}-cloudfront-logs"
+
+if [[ $? -ne 0 ]]; then
+    aws --profile vibrant s3 mb "s3://${bucket}-cloudfront-logs" --region "us-east-1"
 fi
